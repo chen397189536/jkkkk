f60fc9f67734d77008d48642125823a433505ac2
chenyuke <397189536@qq.com>
2016-01-13 15:19:49 +0800

    DDJHD

---
+++ 我的git/我的git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  我的git
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 陈宇科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 我的git/我的git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  我的git
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 陈宇科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
